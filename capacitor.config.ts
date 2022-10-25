8ed660e883a0c538f00e783b25e4b1a2894a6cfe
lmunoz <luis.muoyarzo@gmail.com>
2022-10-24 21:19:50 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'codigo_Qr',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
